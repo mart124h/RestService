9359d78715ad4409ac942d747690f891b331dcaa
mart124h <57940267+mart124h@users.noreply.github.com>
2020-09-29 13:17:44 +0200

    Add files via upload

---
+++ Opgave 4 - RestService/ModelLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModelLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Opgave 4 - RestService/ModelLibrary/model/CykelItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLibrary.model
+{
+    class CykelItem
+    {
+    }
+}
